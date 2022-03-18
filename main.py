bad9b7188965c44f45a2747ffc666147d4dc9af7
mttuan <nmttuan17@gmail.com>
2022-03-18 15:10:02 +0700

    Oops I forgot to let user enter .ics file path

--- main.py
+++ main.py
@@ -20,21 +20,25 @@ import datetime
 from get_timetable import *
 
 def main():
-    first_day = input('Nhập ngày đầu tiên của học kỳ theo định dạng YYYY-MM-DD (Nhập "Y" nếu là năm 2021): ')
-    if first_day == 'Y':
+    os.system('cls')
+    first_day = input('Nhập ngày đầu tiên của học kỳ theo định dạng YYYY-MM-DD (Nhấn Enter nếu là năm 2021): ')
+    if first_day == '':
         first_day = datetime.datetime(year=2021, month=9, day = 20, tzinfo= datetime.datetime.now().astimezone().tzinfo)
     else:
         first_day = datetime.date.fromisoformat(first_day)
-
+    file_path = input('Nhập đường dẫn cho file .ics hoặc nhấn enter để dùng đường dẫn mặc định (/time_table.ics): ')
+    if file_path == '':
+        file_path = 'time_table.ics'
     username = input('Nhập email sis: ')
     os.system('cls')
     password = input('Nhập password: ')
+    os.system('cls')
     opt = Options()
     opt.add_argument('Inprivate')
     driver = webdriver.Edge(options = opt)
     driver.maximize_window()
     time_table = get_timetable(driver, username, password)
-    to_ics(first_day, time_table)
+    to_ics(first_day, time_table, file_path)
 
 if __name__ == '__main__':
     main()
