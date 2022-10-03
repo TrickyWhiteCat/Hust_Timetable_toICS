5f1bdac9f00c106afc317166cceca57d4a33340a
nmttuan <nmttuan17@gmail.com>
2022-10-03 21:58:52 +0700

    Fix incorrect time when using custom start date

--- main.py
+++ main.py
@@ -26,9 +26,10 @@ def main():
     os.system('cls')
     first_day = input('Nhập ngày đầu tiên của năm học theo định dạng YYYY-MM-DD (Nhấn Enter nếu là năm 2021): ')
     if first_day == '':
-        first_day = datetime.datetime(year=2021, month=9, day = 20, tzinfo= datetime.datetime.now().astimezone().tzinfo)
+        first_day = datetime.datetime(year=2021, month=9, day = 20, tzinfo = datetime.datetime.now().astimezone().tzinfo)
     else:
-        first_day = datetime.date.fromisoformat(first_day)
+        date = datetime.date.fromisoformat(first_day)
+        first_day = datetime.datetime(year = date.year, month = date.month, day = date.day, tzinfo = datetime.datetime.now().astimezone().tzinfo)
     file_path = input('Nhập đường dẫn cho file .ics hoặc nhấn enter để dùng đường dẫn mặc định (/time_table.ics): ')
     if file_path == '':
         file_path = 'time_table.ics'
